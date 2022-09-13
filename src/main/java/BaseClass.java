fc2096285f57c6b4a6a46584333a2fcee01d0a44
Shams Uddin <ash.shams15@gmail.com>
2022-09-13 17:27:12 -0400

    Setup the BaseClass

---
+++ src/main/java/BaseClass.java
@@ -0,0 +1,68 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+public class BaseClass {
+
+   public static WebDriver driver;
+
+   public static String getPropertyValue(String propertyName){
+       String propertyValue = "";
+       try {
+           InputStream inputStream = new FileInputStream("");
+           Properties properties = new Properties();
+           properties.load(inputStream);
+       } catch (FileNotFoundException e) {
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+       return propertyValue;
+   }
+
+   public static WebDriver setDriver(){
+       String browserValue = getPropertyValue("browser");
+
+       switch (browserValue.toLowerCase().trim()){
+           case "chrome":
+               WebDriverManager.chromedriver().setup();
+               driver = new ChromeDriver();
+               break;
+           case "firefox":
+               WebDriverManager.firefoxdriver().setup();
+               driver = new FirefoxDriver();
+               break;
+           case "edge":
+               WebDriverManager.edgedriver().setup();
+               driver = new EdgeDriver();
+               break;
+       }
+       return driver;
+   }
+
+
+   @BeforeMethod
+    public void browserSetup(){
+       driver = setDriver();
+       driver.navigate().to(getPropertyValue("url"));
+       driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+   }
+
+   @AfterMethod
+    public void closeBrowser(){
+       driver.close();
+       driver.quit();
+   }
+}
