acef7d4b4c934e43f0dde5d99eb91ba4a92f37b5
Shams Uddin <ash.shams15@gmail.com>
2022-09-13 21:49:19 -0400

    Fixed Browser

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -22,7 +22,7 @@ public class BaseClass {
    public static String getPropertyValue(String propertyName){
        String propertyValue = "";
        try {
-           InputStream inputStream = new FileInputStream("");
+           InputStream inputStream = new FileInputStream("src/main/resources/info.properties");
            Properties properties = new Properties();
            properties.load(inputStream);
        } catch (FileNotFoundException e) {
