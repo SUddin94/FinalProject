3ab6003ce0981a3bc33602214a023ccb3ac8f6ec
yuva <feltanejuba97@gmail.com>
2022-09-21 01:00:48 -0400

    new commit

--- src/main/java/pages/FollowUsPage.java
+++ src/main/java/pages/FollowUsPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import base.BaseClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -16,24 +17,37 @@ public class FollowUsPage extends BaseClass {
         PageFactory.initElements(driver,this);
     }
 
-    @FindBy(linkText = "Facebook") private WebElement facebookLink;
-    @FindBy(linkText = "Twitter") private WebElement twitterLink;
-    @FindBy(linkText = "YouTube") private WebElement youtubeLink;
+    @FindBy(xpath = "//a[@target='_blank']") private List<WebElement> Links;
+    @FindBy(xpath = "//a[@target='_blank']") private List<WebElement> twitterLink;
+    @FindBy(xpath = "//a[@target='_blank']") private List<WebElement> youtubeLink;
 
     public FollowUsPage links(){
-        facebookLink.click();
+        Links.get(0).click();
         Set<String> windowHandles = driver.getWindowHandles();
         List<String> windowHandleList = new ArrayList(windowHandles);
         driver.switchTo().window(windowHandleList.get(1));
+        try{
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         driver.close();
         driver.switchTo().window(windowHandleList.get(0));
-        twitterLink.click();
-        driver.switchTo().window(windowHandleList.get(1));
-        driver.close();
+        driver.switchTo().frame(0);
+        try{
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Links.get(1).click();
         driver.switchTo().window(windowHandleList.get(0));
-        youtubeLink.click();
-        driver.switchTo().window(windowHandleList.get(0));
-        driver.close();
+        driver.switchTo().frame(0);
+        try{
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Links.get(2).click();
         driver.switchTo().window(windowHandleList.get(0));
         return this;
     }
